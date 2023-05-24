31ba43f08fcc3c98a952a8e04bf31a74d717d80a
Vitor Gois <vitorcgois777@gmail.com>
2023-05-23 21:43:44 -0300

    fix: @Repository bean from PlanRepository

--- src/main/java/com/engsoft/coursesapi/infraestructure/repositories/PlanRepository.java
+++ src/main/java/com/engsoft/coursesapi/infraestructure/repositories/PlanRepository.java
@@ -1,11 +1,9 @@
 package com.engsoft.coursesapi.infraestructure.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.engsoft.coursesapi.domain.models.Plan;
 
-@Repository
 public interface PlanRepository extends JpaRepository<Plan, Long> {
 
 }
